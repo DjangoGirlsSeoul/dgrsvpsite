8c9366a86fcfebdb198c1cea4536f2f194e4fe12
Hassan Abid <hassanabidpk89@gmail.com>
2015-08-14 21:12:57 +0900

    minor changes to home page

--- code4every1site/settings.py
+++ code4every1site/settings.py
@@ -102,3 +102,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
