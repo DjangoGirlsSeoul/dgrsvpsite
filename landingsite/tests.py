3b8909b7470b45b875b18db47cde5455e406320c
Jin Park <jin+github@jinpark.net>
2015-08-24 14:14:42 +0900

    update url for test

--- landingsite/tests.py
+++ landingsite/tests.py
@@ -4,7 +4,7 @@ from django.core import mail
 class ContactUsEmailTest(TestCase):
     def test_contact_us_email(self):
         c = Client()
-        c.post("/", {"full_name": "Test Email" ,"email": 'test@example.com', "message": "this is a test message"})
+        c.post("/contact-us/email/", {"full_name": "Test Email" ,"email": 'test@example.com', "message": "this is a test message"})
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, 'Get in touch - Code For Everyone website')
 
