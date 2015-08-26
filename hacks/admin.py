85ec9501451287d5627df2554a32c579ffd0bf6b
Hassan Abid <hassanabidpk89@gmail.com>
2015-08-26 22:37:52 +0900

    fix issue #6 Now slug in hack and category model are auto-created

--- hacks/admin.py
+++ hacks/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Hack, Category
+from django.template.defaultfilters import slugify
 
-admin.site.register(Hack)
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': (slugify('title'),)}
+
+class HackAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': (slugify('title'),)}
+
+admin.site.register(Hack,HackAdmin)
+admin.site.register(Category, CategoryAdmin)
