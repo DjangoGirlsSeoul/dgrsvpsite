ab68721e19cb49be92755521b7afadf119c53846
rachell <rachell.calhoun@gmail.com>
2016-03-08 23:06:28 +0900

    merged gitsite and rsvpsite

--- rsvp/urls.py
+++ rsvp/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^myevents$', views.user_rsvplist, name='user_rsvplist'),
     url(r'^(?P<slug>[-_\w]+)/$', views.event_detail, name='event_detail'),
-    url(r'^$', views.events_list, name='events_list'),
+    url(r'^events$', views.events_list, name='events_list'),
     url(r'^event_signup/(?P<event_id>[0-9]+)/$', views.event_signup, name='event_signup'),
 ]
