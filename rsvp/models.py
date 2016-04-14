fa35cf12d94203526e22e06fe3201d54f60a5656
Hassan Abid <hassanabidpk89@gmail.com>
2016-04-14 23:30:18 +0900

    add webhook for event noti in slack

--- rsvp/models.py
+++ rsvp/models.py
@@ -1,12 +1,21 @@
-from datetime import timedelta
+from datetime import timedelta,datetime,timezone
 
 from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 
 from django_markdown.models import MarkdownField
+import json
+import requests
+import time
+from datetime import timezone
+from django.conf import settings
 
-
+WEBHOOK_URL = settings.WEBHOOK_URL
+payload ={}
+EVENT_TEXT = "A new event(스터디 모임) has been added for {}일 {} - {}. Rsvp <{}|here>"
+EVENT_BASE_URL = "http://www.djangogirlsseoul.org/rsvp/{}/"
+WEEKDAY_KO_ARRAY = {'Monday':'월','Tuesday':'화','Wednesday':'수','Thursday':'목','Friday':'금','Saturday':'토','Sunday':'일'}
 
 class Reservation(models.Model):
     notes =  models.TextField()
@@ -43,5 +52,16 @@ class Event(models.Model):
     def save(self, *args, **kwargs):
         if not self.id:
             self.slug = slugify(self.title)
+            # s = utc_to_local(self.start_time)
+            s = self.start_time
+            weekday_ko = WEEKDAY_KO_ARRAY[s.strftime('%A')]
+            event_start_time = s.strftime('%H:%M')
+            event_month_day = s.strftime('%e')
+            payload["text"] = EVENT_TEXT.format(event_month_day,weekday_ko,event_start_time,EVENT_BASE_URL.format(self.slug))
+            if not settings.DEBUG:
+                payload["channel"] = "#weekend-studygroup"
+            r  = requests.post(WEBHOOK_URL, data = json.dumps(payload))
         return super(Event, self).save(*args, **kwargs)
 
+def utc_to_local(utc_dt):
+    return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
