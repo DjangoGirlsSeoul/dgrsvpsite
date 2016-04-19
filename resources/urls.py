9e6584d63545c2eec4a28464fc82b4c2c418fe72
Hassan Abid <hassanabidpk89@gmail.com>
2016-04-19 13:14:06 +0900

    add resource in db for webhook trigger

--- resources/urls.py
+++ resources/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     # url(r'^(?P<slug>[-_\w]+)/$', views.resourceDetail, name = 'resourceDetail'),
+    url(r'^add/$', views.add_resource, name='add_resource'),
     url(r'^$', views.index, name='index'),
 ]
--- resources/views.py
+++ resources/views.py
@@ -1,10 +1,55 @@
 from django.shortcuts import get_object_or_404,render
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse,Http404,JsonResponse
 from django.template import RequestContext, loader
-
 from .models import Resource,Category
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from django.core.exceptions import ObjectDoesNotExist,MultipleObjectsReturned
 
 def index(request):
     resources = Resource.objects.order_by('-createdAt')
     context = {'resources': resources }
     return render(request,'resources/resources_home.html', context)
+
+@csrf_exempt
+def add_resource(request):
+    if request.method == 'POST':
+        responseJson = {}
+        token = request.POST['token']
+        channel_name = request.POST['channel_name']
+        trigger_word = request.POST['trigger_word']
+        if token == settings.WEBHOOK_TOKEN and channel_name == "learning-resources" and trigger_word == "resource":
+            user_name = request.POST['user_name']
+            timestamp = request.POST['timestamp']
+            text = request.POST['text']
+            print(user_name,text,trigger_word)
+            result = saveResource(text)
+            responseJson["text"] = result.format(user_name)
+            response = JsonResponse(responseJson)
+            return response
+        else :
+            print("token does not match")
+
+def saveResource(text):
+    textArray = text.split()
+    #ref: ['resource', '-title:', 'visualgo', 'category:', 'algorithm', 'link:', 'http://visualgo.net/']
+    if len(textArray) >= 7 and "http" in textArray[6]:
+        link = textArray[6]
+        print(link)
+        category = textArray[4].lower()
+        try:
+            cat = Category.objects.get(title=category)
+            print("cat :",cat)
+        except ObjectDoesNotExist:
+            cat = Category(title=category)
+            cat.save()
+        try:
+            resource = Resource.objects.get(link=link)
+            return "Resource saved already @{}"
+        except ObjectDoesNotExist:
+            resource = Resource(title=textArray[2],link=link,category=cat)
+            resource.save()
+            return "Resource successfully saved @{}"
+
+    else :
+        return "This resource is not properly formatted @{}"
