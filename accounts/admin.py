611f89086c41a071c621037bf73923875e64b140
Jin Park <jin+github@jinpark.net>
2016-06-25 16:59:01 +0900

    make error actually look like an error

--- accounts/admin.py
+++ accounts/admin.py
@@ -23,7 +23,7 @@ class CustomUserAdmin(UserAdmin):
             try:
                 email.send()
             except Exception as e:
-                self.message_user(request, "Sending Email failed for {} - {}".format(user.username, e))
+                self.message_user(request, "Sending Email failed for {} - {}".format(user.username, e), 'error')
 
     approve_and_email.short_description = "Approve and Email User(s)"
 
